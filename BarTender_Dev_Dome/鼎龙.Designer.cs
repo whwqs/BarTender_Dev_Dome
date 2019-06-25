f2b6ab339911fced468d2a4d885b4ef3f1cf1eaf
whwqs <whwqs@hotmail.com>
2019-06-25 11:03:06 +0800

    去掉vs文件夹

---
+++ BarTender_Dev_Dome/鼎龙.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace BarTender_Dev_Dome
+{
+	partial class 鼎龙
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtN = new System.Windows.Forms.TextBox();
+			this.txtK = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtD = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtB = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.chkPrint = new System.Windows.Forms.CheckBox();
+			this.lstBox = new System.Windows.Forms.ListBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(884, 51);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "载入btw";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(853, 140);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "编号：";
+			// 
+			// txtN
+			// 
+			this.txtN.Location = new System.Drawing.Point(901, 130);
+			this.txtN.Name = "txtN";
+			this.txtN.Size = new System.Drawing.Size(211, 21);
+			this.txtN.TabIndex = 3;
+			this.txtN.Text = "NTS2398766";
+			// 
+			// txtK
+			// 
+			this.txtK.Location = new System.Drawing.Point(901, 186);
+			this.txtK.Name = "txtK";
+			this.txtK.Size = new System.Drawing.Size(211, 21);
+			this.txtK.TabIndex = 5;
+			this.txtK.Text = "10kg";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(853, 196);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "重量：";
+			// 
+			// txtD
+			// 
+			this.txtD.Location = new System.Drawing.Point(901, 243);
+			this.txtD.Name = "txtD";
+			this.txtD.Size = new System.Drawing.Size(211, 21);
+			this.txtD.TabIndex = 7;
+			this.txtD.Text = "9月8日";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(853, 253);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(41, 12);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "日期：";
+			// 
+			// txtB
+			// 
+			this.txtB.Location = new System.Drawing.Point(901, 313);
+			this.txtB.Name = "txtB";
+			this.txtB.Size = new System.Drawing.Size(211, 21);
+			this.txtB.TabIndex = 9;
+			this.txtB.Text = "900456789";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(853, 323);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(41, 12);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "条码：";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(884, 388);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "加载数据";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// chkPrint
+			// 
+			this.chkPrint.AutoSize = true;
+			this.chkPrint.Location = new System.Drawing.Point(974, 392);
+			this.chkPrint.Name = "chkPrint";
+			this.chkPrint.Size = new System.Drawing.Size(84, 16);
+			this.chkPrint.TabIndex = 11;
+			this.chkPrint.Text = "加载后打印";
+			this.chkPrint.UseVisualStyleBackColor = true;
+			// 
+			// lstBox
+			// 
+			this.lstBox.FormattingEnabled = true;
+			this.lstBox.ItemHeight = 12;
+			this.lstBox.Location = new System.Drawing.Point(916, 431);
+			this.lstBox.Name = "lstBox";
+			this.lstBox.Size = new System.Drawing.Size(211, 172);
+			this.lstBox.TabIndex = 12;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(833, 442);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(77, 12);
+			this.label5.TabIndex = 13;
+			this.label5.Text = "打印机列表：";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(835, 614);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(96, 23);
+			this.button3.TabIndex = 14;
+			this.button3.Text = "打印多个标签";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// 鼎龙
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1139, 649);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.lstBox);
+			this.Controls.Add(this.chkPrint);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.txtB);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.txtD);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtK);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtN);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "鼎龙";
+			this.Text = "鼎龙";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtN;
+		private System.Windows.Forms.TextBox txtK;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtD;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtB;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.CheckBox chkPrint;
+		private System.Windows.Forms.ListBox lstBox;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button3;
+	}
+}
---
+++ BarTender_Dev_Dome/鼎龙.cs
@@ -0,0 +1,101 @@
+﻿using Seagull.BarTender.Print;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarTender_Dev_Dome
+{
+	public partial class 鼎龙 : Form
+	{
+		public 鼎龙()
+		{
+			InitializeComponent();
+
+			Printers printers = new Printers();
+			lstBox.Items.Clear();
+			foreach (Printer printer in printers)
+			{
+				lstBox.Items.Add(printer.PrinterName);
+			}
+		}
+
+		string btwfile;
+
+		string jpgtmp = Path.Combine(Application.StartupPath, "tmp.jpg");
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				btwfile = openFileDialog1.FileName;
+				pictureBox1.Image = null;
+				string jpgtmp = Path.Combine(Application.StartupPath, "tmp.jpg");
+				using (Engine btEngine = new Engine(true))
+				{
+					LabelFormatDocument labelFormat = btEngine.Documents.Open(btwfile);
+					labelFormat.ExportImageToFile(jpgtmp, ImageType.JPEG, Seagull.BarTender.Print.ColorDepth.ColorDepth24bit, new Resolution(800, 600), OverwriteOptions.Overwrite);
+
+					Image image = Image.FromFile(jpgtmp);
+					Bitmap NmpImage = new Bitmap(image);
+					pictureBox1.Image = NmpImage;
+					image.Dispose();				
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			using (Engine btEngine = new Engine(true))
+			{
+				LabelFormatDocument labelFormat = btEngine.Documents.Open(btwfile);
+				try
+				{					
+					labelFormat.SubStrings.SetSubString("No", txtN.Text);
+					labelFormat.SubStrings.SetSubString("日期", txtD.Text);
+					labelFormat.SubStrings.SetSubString("重量", txtK.Text);
+					labelFormat.SubStrings.SetSubString("条码", txtB.Text);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("修改内容出错 " + ex.Message, "操作提示");
+					return;
+				}
+
+				labelFormat.ExportImageToFile(jpgtmp, ImageType.JPEG, Seagull.BarTender.Print.ColorDepth.ColorDepth24bit, new Resolution(800, 600), OverwriteOptions.Overwrite);
+
+				Image image = Image.FromFile(jpgtmp);
+				Bitmap NmpImage = new Bitmap(image);
+				pictureBox1.Image = NmpImage;
+				image.Dispose();
+
+				if (chkPrint.Checked)
+				{
+					if (lstBox.SelectedItem == null)
+					{
+						MessageBox.Show("请选择打印机");
+						return;
+					}
+					string printerName = lstBox.SelectedItem.ToString();
+
+					labelFormat.PrintSetup.PrinterName = printerName;
+
+					string jobName = "BarPrint" + DateTime.Now;
+
+					labelFormat.Print(jobName);
+				}
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
